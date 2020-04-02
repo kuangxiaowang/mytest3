34ee224c5b01e3973833808a6ca216e1ad6204a0
kuangxiaowang <63009517+kuangxiaowang@users.noreply.github.com>
2020-04-02 22:57:44 +0800

    创建js1文件

---
+++ js1.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>JavaScript 字符串方法</h1>
+
+<p id="demo"></p>
+
+<script>
+var text = "";
+text = text + "H"+"<br>";
+  text = text + "E"+"<br>";
+ document.getElementById("demo").innerHTML = text;
+</script>
+
+</body>
+</html>
